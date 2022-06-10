69ce5e2bab153f4365048fd78c2b4443cb8b9100
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-06-10 20:53:26 +0530

    Add files via upload

---
+++ tut4.js
@@ -0,0 +1,53 @@
+//string
+let name = "Niket";
+console.log("My name is "+ name);
+console.log("data type is "+ (typeof name));
+
+//number
+let marks = 90;
+console.log("My marks is "+ marks);
+console.log("data type is "+ (typeof marks));
+
+//boolean
+let isDriver = true;
+console.log("data type is "+(typeof isDriver));
+
+//null
+let nullVar = null;
+console.log("data type is "+(typeof nullVar));
+
+//undefined
+let undef = undefined;
+console.log("data type is "+(typeof undef));
+
+
+
+//reference data types//
+
+//arrays
+let myarr = [1,2,5,6,7, true, false, "string"];
+console.log("data type is "+(typeof myarr));
+
+//object literals
+let scores = {
+    harry: 90,
+    niket: 89,
+    pawan: 75 //do not put spaces b/w key
+}
+console.log(scores);
+console.log(typeof scores);
+
+
+//function
+function findname() {
+    
+}
+console.log(typeof findname);
+
+//date
+let date = new Date();
+console.log(typeof date);
+
+
+
+
